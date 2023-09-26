1f397365b85a49db73a9922ee8a0ef73f55aacc2
IdoCy <138014292+IdoCy@users.noreply.github.com>
2023-09-26 14:45:22 +0300

    Update main.py (#27)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print("My secret password is HeyUWhayUGonnaDoPassword4?$")
 password = "MyVery Secret Password r$$PRED"
 real_pass = Hard6TellPassword423#@password4
+var  = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEEEY
+var2 = "wJalrXUtnFEMI/K7MDENG/bPxRsxCYEXAMPLEKEY"
